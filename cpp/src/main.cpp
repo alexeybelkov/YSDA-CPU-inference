6ba08f2ca75a7defc547c118dd8be8ad1992af47
Alexey Belkov <fpmbelkov@gmail.com>
2023-10-29 03:16:52 +0300

    add grpof

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,5 +3,7 @@
 #include <torch/torch.h>
 
 int main() {
-    at::linear;
+    torch::Tensor weight = torch::randn({128, 256, 3, 3, 3});
+    torch::Tensor input = torch::randn({1, 256, 4, 4, 4});
+    at::conv3d(input, weight);
 }
