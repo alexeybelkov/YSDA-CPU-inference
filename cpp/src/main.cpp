694734d071a932be5e75c76c7546e21451e95047
Alexey Belkov <fpmbelkov@gmail.com>
2023-11-01 03:59:25 +0300

    basic profilinf, pre new-branch commit

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,7 +3,13 @@
 #include <torch/torch.h>
 
 int main() {
-    torch::Tensor weight = torch::randn({128, 256, 3, 3, 3});
-    torch::Tensor input = torch::randn({1, 256, 4, 4, 4});
-    at::conv3d(input, weight);
+    at::Tensor weight = at::rand({128, 256, 3, 3, 3});
+    at::Tensor input = at::rand({1, 256, 64, 64, 64});
+    // at::Tensor weight = at::rand({1024, 512});
+    // at::Tensor input = at::rand({1, 512});
+    at::Tensor output;
+    output = at::conv3d(input, weight);
+    // auto x = at::rand({64, 64}), y = at::rand({64, 64}), z = at::rand({64, 64});
+    // output = at::native::linear(x, y, z);
+    std::cout << output[0][0];
 }
