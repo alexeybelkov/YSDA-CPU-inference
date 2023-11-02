c470b37a440682900c1df40ed20f6b51be76fd71
Alexey Belkov <fpmbelkov@gmail.com>
2023-11-02 16:26:40 +0300

    CMakeLists changes

---
+++ cpp/src/aten.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "ATen/native/LinearAlgebra.h"
+
+int main() {
+    std::cout << "HELLO\n";
+    return 0;
+}
---
+++ cpp/src/libtorch.cpp
@@ -0,0 +1,23 @@
+#include <ATen/ATen.h>
+#include <iostream>
+#include <torch/torch.h>
+
+int main() {
+    // at::Tensor weight = at::rand({128, 256, 3, 3, 3}).to_mkldnn();
+    // at::Tensor input = at::rand({1, 256, 64, 64, 64}).to_mkldnn();
+    at::Tensor weight = at::rand({1024, 512}).to_mkldnn();
+    at::Tensor input = at::rand({1, 512}).to_mkldnn();
+    // at::Tensor output;
+    // // output = at::conv3d(input, weight);
+    // // auto x = at::rand({64, 64}), y = at::rand({64, 64}), z = at::rand({64, 64});
+    // output = at::native::linear(input, weight);
+
+    at::Tensor m1 = at::rand({64, 64}), m2 = at::rand({64, 64});
+    at::Tensor result = at::zeros_like(m1);
+
+    at::Tensor output = at::mkldnn_linear(input, weight);
+    std::cout << output.size(0);
+    
+
+    // std::cout << output[0][0];
+}  
