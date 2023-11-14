3f9bc0049d1178e65315a88ce6cd79f21bc85808
Alexey Belkov <fpmbelkov@gmail.com>
2023-11-15 02:13:39 +0300

    microhange

--- cpp/src/pytorch.cpp
+++ cpp/src/pytorch.cpp
@@ -4,7 +4,11 @@
 #include <torch/torch.h>
 
 int main() {
-    // mm -> mm::respatch -> /home/alexey/YSDA/YSDA-CPU-inference/cpp/pytorch-build/aten/src/ATen/Operators_3.cpp:3896
+    // mm -> mm::respatch -> /home/alexey/YSDA/YSDA-CPU-inference/cpp/pytorch-build/aten/src/ATen/Operators_3.cpp:3896 -> 
+    // -> /home/alexey/YSDA/YSDA-CPU-inference/cpp/pytorch-build/aten/src/ATen/RegisterCPU.cpp:8640 -> 
+    // -> /home/alexey/YSDA/YSDA-CPU-inference/cpp/pytorch/aten/src/ATen/native/LinearAlgebra.cpp:1383 -> 
+    // ->  /home/alexey/YSDA/YSDA-CPU-inference/cpp/pytorch/aten/src/ATen/native/LinearAlgebra.cpp:1513 -> 
+    // ??? /home/alexey/YSDA/YSDA-CPU-inference/cpp/pytorch/aten/src/ATen/native/CPUBlas.cpp:161
     at::Tensor weight = at::rand({32, 16}, at::requires_grad(false));
     // weight.set_requires_grad(false);
     at::Tensor input = at::rand({4, 32}, at::requires_grad(false));
